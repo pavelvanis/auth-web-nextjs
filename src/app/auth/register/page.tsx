338bdf198b76be2cdae62160e638a49dcfefe1ba
vanis <pavlik.vanis@email.cz>
2024-03-04 13:15:53 +0100

    Create page for register

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "./_register-form";
+
+const RegisterPage = () => {
+  return (
+    <main className="">
+      <RegisterForm />
+    </main>
+  );
+};
+
+export default RegisterPage;
